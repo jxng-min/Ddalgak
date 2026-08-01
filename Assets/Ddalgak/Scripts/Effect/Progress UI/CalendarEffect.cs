f53613b3bf305cb63d75e0192414ee28e6b1816d
Jongmin Kim <vwmartin@naver.com>
2026-08-01 21:44:28 +0900

    feat: Progress UI 구현

---
+++ Assets/Ddalgak/Scripts/Effect/Progress UI/CalendarEffect.cs
@@ -0,0 +1,47 @@
+﻿using DG.Tweening;
+using JxModule;
+using UnityEngine;
+
+namespace Ddalgak
+{
+    [ManagedEffect("Progress", "Calendar", 1)]
+    public class CalendarEffect : MonoBehaviour
+    {
+        [BigHeader("settings")]
+        [Header("Initialize Settings")]
+        [SerializeField] private float initXOffset;
+        [SerializeField] private float initDuration;
+        [SerializeField] private Ease initEase = Ease.OutBack;
+        
+        [Header("Update Settings")]
+        [SerializeField] private float updateXOffset;
+        [SerializeField] private float updateDuration;
+        [SerializeField] private float waitDuration;
+        [SerializeField] private Ease updateEase = Ease.OutBack;
+
+        public Tween PlayInitCalendarEffect(RectTransform calendarRect, Vector2 originAnchoredPosition)
+        {
+            return calendarRect.DOAnchorPosX(originAnchoredPosition.x - initXOffset, initDuration)
+                               .SetEase(initEase);
+        }
+
+        public Tween PlayUpdateCalendarEffect(RectTransform calendarRect, Vector2 originAnchoredPosition)
+        {
+            
+            var sequence = DOTween.Sequence();
+
+            sequence.Join(
+                calendarRect.DOAnchorPosX(originAnchoredPosition.x - updateXOffset, updateDuration)
+                            .SetEase(updateEase)
+            );
+
+            sequence.AppendInterval(waitDuration);
+
+            sequence.Append(
+                calendarRect.DOAnchorPosX(originAnchoredPosition.x - initXOffset, updateDuration)
+            );
+
+            return sequence;
+        }
+    }
+}
---
+++ Assets/Ddalgak/Scripts/Presenters/ProgressPresenter.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using JxModule;
+using UnityEngine;
+
+namespace Ddalgak
+{
+    public class ProgressPresenter : ProgressPresenterBase
+    {
+        [BigHeader("UI")]
+        [SerializeField] private BackgroundView backgroundView;
+        [SerializeField] private CalendarView calendarView;
+        [SerializeField] private StatView statView;
+        
+        public override IEnumerator ShowGameStart(GameRuntimeState runtimeState)
+        {
+            yield return backgroundView.DrawKingdom();
+        }
+
+        public override IEnumerator ShowWeekStart(GameRuntimeState runtimeState)
+        {
+            yield return statView.InitStat();
+            yield return calendarView.InitWeek();
+        }
+
+        public override IEnumerator ShowWeekSettlement(GameRuntimeState runtimeState)
+        {
+            yield return calendarView.UpdateWeek(runtimeState);
+        }
+
+        public override void Cancel() {}
+    }
+}
