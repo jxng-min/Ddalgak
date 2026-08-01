4b2998b3dead85904cc5d2a71758c7ca625ec4b3
Jongmin Kim <vwmartin@naver.com>
2026-08-01 22:03:55 +0900

    fix: Event DataTable이 Texture를 받도록 수정

--- Assets/Ddalgak/Scripts/DataTableRows/EventDataTableRow.cs
+++ Assets/Ddalgak/Scripts/DataTableRows/EventDataTableRow.cs
@@ -1,4 +1,5 @@
 using System.Collections.Generic;
+using JxModule;
 using JxModule.DataTable;
 using UnityEngine;
 
@@ -9,6 +10,7 @@ namespace Ddalgak
         public string eventName;
         public string title;
         public string description;
+        public Texture2D eventTexture;
         public EEventType eventType;
         public float weight;
         public bool isConditional;
@@ -38,6 +40,7 @@ namespace Ddalgak
                 eventName = eventName,
                 title = title,
                 description = description,
+                eventImage = eventTexture.ToSprite(),
                 eventType = eventType,
                 weight = weight,
                 isConditional = isConditional,
